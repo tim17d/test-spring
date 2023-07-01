4a4f373eecfd44ecd192aa78d4a0e7126f3f7977
Timur Zhemaldinov <tim17d92@gmail.com>
2023-07-01 22:44:00 +0300

    Test bean lifecycle and init-destroy-factory methods

--- src/main/java/ClassicalMusic.java
+++ src/main/java/ClassicalMusic.java
@@ -1,5 +1,20 @@
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    private void doInit() {
+        System.out.println("Initialize ClassicalMusic Bean");
+    }
+
+    private void doDestroy() {
+        System.out.println("Destroy ClassicalMusic Bean");
+    }
+
     @Override
     public String getSong() {
         return "Caprice No.24";
--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -4,8 +4,12 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application_context.xml");
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
-        player.playMusic(3);
+        ClassicalMusic music1 = context.getBean("classicalMusicBean", ClassicalMusic.class);
+        ClassicalMusic music2 = context.getBean("classicalMusicBean", ClassicalMusic.class);
+        ClassicalMusic music3 = context.getBean("classicalMusicBean", ClassicalMusic.class);
+
+//        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+//        player.playMusic(2);
         context.close();
     }
 }
